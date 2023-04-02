72e4f3125c2751764266db5550b9fb6cbda92ae2
Nirwash <nirwash@bk.ru>
2023-04-02 14:17:00 +0300

    refactoring

--- app/src/main/java/com/nirwashh/astonintensive5/Contact.java
+++ app/src/main/java/com/nirwashh/astonintensive5/Contact.java
@@ -1,10 +1,7 @@
 package com.nirwashh.astonintensive5;
 
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class Contact implements Parcelable {
+public class Contact {
     String name;
     String lastname;
     String telNumber;
@@ -16,36 +13,4 @@ public class Contact implements Parcelable {
         this.telNumber = telNumber;
         this.id = id;
     }
-
-    protected Contact(Parcel in) {
-        name = in.readString();
-        lastname = in.readString();
-        telNumber = in.readString();
-        id = in.readInt();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeString(lastname);
-        dest.writeString(telNumber);
-        dest.writeInt(id);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
-        @Override
-        public Contact createFromParcel(Parcel in) {
-            return new Contact(in);
-        }
-
-        @Override
-        public Contact[] newArray(int size) {
-            return new Contact[size];
-        }
-    };
 }
